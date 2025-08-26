6ef6e3173a3bf1d6880216796818312829c220fb
Safintc <safintc7899@gmail.com>
2025-08-26 17:38:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,9 +12,9 @@ import os
 
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_Config import Config
+    from sample_config import Config
 else:
-    from Config import Config
+    from config import Config
 
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
